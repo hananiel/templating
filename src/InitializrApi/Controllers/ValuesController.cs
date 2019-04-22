6d0136e6b00057c34048aabb77c0ac0430a82b9d
Hananiel Sarella <hananiel@gmail.com>
2019-04-22 13:41:16 -0400

    Api

---
+++ src/InitializrApi/Controllers/ValuesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Cli;
+using Microsoft.TemplateEngine.Cli.PostActionProcessors;
+using Microsoft.TemplateEngine.Edge;
+using Microsoft.TemplateEngine.Edge.TemplateUpdates;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects;
+using Microsoft.TemplateEngine.Utils;
+
+namespace InitializrApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private const string HostIdentifier = "dotnetcli";
+        private const string HostVersion = "1.0.0";
+        private const string CommandName = "new3";
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var host = CreateHost(false);
+             New3Command.Run(CommandName, host, new TelemetryLogger(null, true), FirstRun,
+                 new[]{"console" });
+            Console.WriteLine("stuff happened");
+             return new List<string> {"success"};
+        }
+ private static DefaultTemplateEngineHost CreateHost(bool emitTimings)
+        {
+            var preferences = new Dictionary<string, string>
+            {
+                { "prefs:language", "C#" }
+            };
+
+            try
+            {
+                string versionString = Dotnet.Version().CaptureStdOut().Execute().StdOut;
+                if (!string.IsNullOrWhiteSpace(versionString))
+                {
+                    preferences["dotnet-cli-version"] = versionString.Trim();
+                }
+            }
+            catch
+            { }
+
+            var builtIns = new AssemblyComponentCatalog(new[]
+            {
+                typeof(RunnableProjectGenerator).GetTypeInfo().Assembly,            // for assembly: Microsoft.TemplateEngine.Orchestrator.RunnableProjects
+                typeof(NupkgInstallUnitDescriptorFactory).GetTypeInfo().Assembly,   // for assembly: Microsoft.TemplateEngine.Edge
+                typeof(DotnetRestorePostActionProcessor).GetTypeInfo().Assembly     // for assembly: Microsoft.TemplateEngine.Cli
+            });
+
+            DefaultTemplateEngineHost host = new DefaultTemplateEngineHost(HostIdentifier, HostVersion, CultureInfo.CurrentCulture.Name, preferences, builtIns, new[] { "dotnetcli" });
+
+            if (emitTimings)
+            {
+                host.OnLogTiming = (label, duration, depth) =>
+                {
+                    string indent = string.Join("", Enumerable.Repeat("  ", depth));
+                    Console.WriteLine($"{indent} {label} {duration.TotalMilliseconds}");
+                };
+            }
+
+
+            return host;
+        }
+        private static void FirstRun(IEngineEnvironmentSettings environmentSettings, IInstaller installer)
+        {
+            string baseDir = Environment.ExpandEnvironmentVariables("%DN3%");
+
+            if (baseDir.Contains('%'))
+            {
+                Assembly a = typeof(Program).GetTypeInfo().Assembly;
+                string path = new Uri(a.CodeBase, UriKind.Absolute).LocalPath;
+                path = Path.GetDirectoryName(path);
+                Environment.SetEnvironmentVariable("DN3", path);
+            }
+
+            List<string> toInstallList = new List<string>();
+            Paths paths = new Paths(environmentSettings);
+
+            if (paths.FileExists(paths.Global.DefaultInstallPackageList))
+            {
+                toInstallList.AddRange(paths.ReadAllText(paths.Global.DefaultInstallPackageList).Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries));
+            }
+
+            if (paths.FileExists(paths.Global.DefaultInstallTemplateList))
+            {
+                toInstallList.AddRange(paths.ReadAllText(paths.Global.DefaultInstallTemplateList).Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries));
+            }
+
+            if (toInstallList.Count > 0)
+            {
+                for (int i = 0; i < toInstallList.Count; i++)
+                {
+                    toInstallList[i] = toInstallList[i].Replace("\r", "")
+                        .Replace('\\', Path.DirectorySeparatorChar);
+                }
+
+                installer.InstallPackages(toInstallList);
+            }
+        }
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
